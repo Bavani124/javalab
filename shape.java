8da7de20b13f0dedf91de1ef20d76dc34431f3cd
Bavani124 <bhavanigolagana04@gmail.com>
2024-10-30 09:37:33 +0530

    Create shape.java

---
+++ shape.java
@@ -0,0 +1,81 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class Shape
+{
+    String color;
+    boolean filled;
+    Shape()
+    {
+        color="green";
+        filled=true;
+    }
+    Shape(String color,boolean filled)
+    {
+        this.color=color;
+        this.filled=filled;
+    }
+    String getColor()
+    {
+        return color;
+    }
+    void setColor(String color)
+    {
+        this.color=color;
+    }
+    boolean isFilled()
+    {
+        return filled;
+    }
+    void setFilled(boolean filled)
+    {
+        this.filled=filled;
+    }
+}
+class Circle extends Shape
+{
+    double radius;
+    Circle(double radius)
+    {
+        this.radius=radius;
+    }
+    Circle(double radius,String color,boolean filled)
+    {
+        super(color,filled);
+        this.radius=radius;
+    }
+    double getRadius()
+    {
+        return radius;
+    }
+    void setRadius(double radius)
+    {
+        this.radius=radius;
+    }
+    double getArea()
+    {
+        return 3.14*radius*radius;
+    }
+    double getPerimeter()
+    {
+        return 2*radius*3.14;
+    }
+    void display()
+    {
+        System.out.println("Circle with radius "+radius+" has area "+getArea()+" and perimeter "+getPerimeter());
+        System.out.println("Color: "+color+" and filled: "+filled);
+    }
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Circle c=new Circle(5.0, "red", true);
+        c.display();
+
+    }
+}
